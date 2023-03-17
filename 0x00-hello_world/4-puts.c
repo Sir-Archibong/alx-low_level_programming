8f38fa145fe5050c0be23d45bd9f60b15dfdaf5f
Sir-Archibong <itoro.archibong01@gmail.com>
2023-03-17 02:58:23 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Prints a string with puts
+ *
+ * Return: 0 if program executed successfully
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
