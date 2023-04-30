9feedfbf862485a3d6efaef36722913e0170018e
Sir-Archibong <itoro.archibong01@gmail.com>
2023-04-30 15:29:03 +0100

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,30 +2,33 @@
 
 /**
  * find_listint_loop - finds the loop in a linked list
- * @head: pointer to head of list
- * Return: address of node where loop starts
+ *
+ * @head: the listint_t argument (head)
+ *
+ * Return: the address of the node where the loop starts, or NULL if no loop
  */
-listint_t *find_listint_loop(listint_t *head)
-{
-	listint_t *tortoise;
-	listint_t *hare;
 
-	if (!head)
-		return (NULL);
-	tortoise = hare = head;
-	while (hare->next && (hare->next)->next)
+listint_t	*find_listint_loop(listint_t *head)
+{
+	listint_t *slow_p = head, *fast_p = head;
+
+	if (head && head->next)
 	{
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
-		if (tortoise == hare)
+		while (slow_p && fast_p && fast_p->next)
 		{
-			tortoise = head;
-			while (tortoise != hare)
+			slow_p = slow_p->next;
+			fast_p = fast_p->next->next;
+
+			if (slow_p == fast_p)
 			{
-				tortoise = tortoise->next;
-				hare = hare->next;
+				slow_p = head;
+				while (slow_p != fast_p)
+				{
+					slow_p = slow_p->next;
+					fast_p = fast_p->next;
+				}
+				return (slow_p);
 			}
-			return (hare);
 		}
 	}
 	return (NULL);
